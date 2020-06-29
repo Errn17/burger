86fc64369f1f4a1cef170bc29240361352320e9b
Aaron <aaronkh19@gmail.com>
2020-06-28 18:30:05 -0600

    changed seeds.sql code and added data to burgers_db

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,10 @@
---Drop Database if it already exists
+
 DROP DATABASE IF EXISTS burgers_db;
 
---Creating the database
 CREATE DATABASE burgers_db;
 
---Using this database
 USE burgers_db;
 
---Creating burgers table
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger VARCHAR(100),
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,2 @@
-
-INSERT INTO burgers
-VALUES ("Cheeseburger", 0), ("Bbq Burger", 1), ("Bacon Cheeseburger" 0), ("Burger" 0)
+USE burgers_db;
+INSERT INTO burgers (burger, devoured) VALUES ("Cheeseburger", 0),("Bbq Burger", 1),("Bacon Cheeseburger", 0),("Burger", 0);
