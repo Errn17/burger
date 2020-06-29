553051136a4bcc7ca9dc60e0541040ba039d4bd7
Aaron <aaronkh19@gmail.com>
2020-06-28 18:45:55 -0600

    setup connection.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Shotei1717!",
+  database: "burgers_db",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error you are not connected to the database" + err.stack);
+    return;
+  }
+  console.log("Connected as ID" + connection.threadId);
+});
