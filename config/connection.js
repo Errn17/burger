f77f65c807e6725b84e9a9561c0b226a004bd20a
Aaron <aaronkh19@gmail.com>
2020-06-30 15:00:31 -0600

    trying to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,32 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Shotei1717!",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Shotei1717!",
+    database: "burgers_db",
+  });
+}
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Error you are not connected to the database" + err.stack);
-    return;
-  }
-  console.log("Connected as ID" + connection.threadId);
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Shotei1717!",
+//   database: "burgers_db",
+// });
+// connection.connect((err) => {
+//   if (err) {
+//     console.log("Error you are not connected to the database" + err.stack);
+//     return;
+//   }
+//   console.log("Connected as ID" + connection.threadId);
+// });
+
+connection.connect();
 
 module.exports = connection;
