688941ee21d8506c0f110ffc8ae589a12b82de56
Aaron <aaronkh19@gmail.com>
2020-06-30 14:08:15 -0600

    finished everything up with help from youtube

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const PORT = process.env.PORT || 8080;
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Live on http://localhost:${PORT}`);
+});
